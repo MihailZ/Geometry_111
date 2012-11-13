aee2580056e7ea924c2888e031886d5551db9eee
Mihailz <zig-92@ya.ru>
2012-11-13 12:18:20 +0200

    classes Circle->Ellipse, Trapezoid, Rectangle and unit-tests for them has been created

--- geom.rb
+++ geom.rb
@@ -1,3 +1,33 @@
+class Circle
+ def initialize(r)
+  @r = r
+ end
+
+ def length
+  @r * 2 * Math::PI
+ end
+
+ def area
+  @r * @r * Math::PI
+ end
+end
+
+class Ellipse < Circle
+ def initialize(r1,r2)
+  @r,@r1 = r1,r2
+ end
+
+ def length
+  (@r + @r1) * Math::PI
+ end
+
+ def area
+  @r * @r1 * Math::PI
+ end
+
+end
+
+
 class Quadrilateral
  def initialize(a,b,c,d)
   @a,@b,@c,@d = a,b,c,d 
@@ -15,11 +45,7 @@ end
 
 class Parallelogram < Quadrilateral
  def initialize(a,b,angle)
-  @a = a
-  @b = b
-  @c = a
-  @d = b
-  @ang = angle
+  @a,@b,@c,@d,@ang = a,b,a,b,angle 
  end
  
  def perimetr 
@@ -34,11 +60,7 @@ end
 
 class Rhombus < Quadrilateral
  def initialize(a, angle)
-  @a = a
-  @b = a
-  @c = a
-  @d = a
-  @ang = angle
+  @a,@b,@c,@d,@ang = a,a,a,a,angle
  end
  
  def area
@@ -51,18 +73,65 @@ class Rhombus < Quadrilateral
  end
 end
 
+class Trapezoid < Quadrilateral
+ def initialize(a,b,c,d,h)
+  @a,@b,@c,@d,@h = a,b,c,d,h 
+ end
+
+ def area
+  (@a + @c)*@h/2
+ end
+end
+
+class Rectangle < Quadrilateral
+ def initialize(a,b)
+  @a,@b,@c,@d = a,b,a,b
+ end
+
+ def perimetr
+  2 * (@a + @b)
+ end
+
+ def area
+  @a * @b
+ end
+end
+  
+
+
 printf ("Quad \n")
-quad = Quadrilateral.new(2,3,4,5)
+quad = Quadrilateral.new(2,3,4,5)#a,b,c,d - segment
 p quad.perimetr() 
 p quad.area() 
 
 #град = 180 * рад / пи
 printf ("Parall \n")
-paral = Parallelogram.new(2,3,90)
+paral = Parallelogram.new(2,3,60)#a,b - segment, angle
 p paral.area()
 p paral.perimetr()
 
 printf ("Romb \n")
-romb = Rhombus.new(2,90)
+romb = Rhombus.new(2,90)#a - segment, angle
 p romb.perimetr()
 p romb.area()
+
+printf ("Trapezia \n")
+trap = Trapezoid.new(2,3,4,4,3) #a,b,c,d - segment; h - hight
+p trap.perimetr() #using parent method
+p trap.area()
+
+printf ("Rectangle \n")
+rec = Rectangle.new(1,3)#a,b - segment
+p rec.perimetr()
+p rec.area()
+
+printf ("Circle \n")
+cir = Circle.new(4)#r - radius
+p cir.length()
+p cir.area()
+
+printf ("Ellipse \n")
+el = Ellipse.new(3,2)#r1,r2 - radius(min and max)
+p el.length()
+p el.area()
+
--- spec/geom_spec.rb
+++ spec/geom_spec.rb
@@ -1,11 +1,62 @@
 requier 'spec_helper.rb'
+ describe Circle
+  let(:cir) {Circle.new(2)}
+
+	describe "#initialize" do
+		it "should raise an ArgumentError error if no parameters passed" do
+			expect { Circle0.new(2,3) }.to raise_error(ArgumentError)
+		end
+	end 
+ 
+	describe "#length" do
+		it "should count length of circle"
+		  length_chkd = 2 * 2 * Math::PI
+		  length = cir.length
+		  expect(length).should eq length_chkd
+		end
+	end	
+	
+	describe "#area" do
+		it "should count area of circle"
+		 area_chkd = 2*2*Math::PI
+		 area = cir.area
+		 expect(area).should eq area_chkd
+		end
+	end
+ end
+ 
+ describe Ellipse
+  let(:el) {Ellipse.new(1,2)}
+
+	describe "#initialize" do
+		it "should raise an ArgumentError error if no parameters passed" do
+			expect { Ellipse.new(2,3,2) }.to raise_error(ArgumentError)
+		end
+	end 
+ 
+	describe "#length" do
+		it "should count length of ellipse"
+		  length_chkd = (2 + 1)* Math::PI
+		  length = el.length
+		  expect(length).should eq length_chkd
+		end
+	end	
+	
+	describe "#area" do
+		it "should count area of ellipse"
+		 area_chkd = 2*1*Math::PI
+		 area = el.area
+		 expect(area).should eq area_chkd
+		end
+	end
+ end		
 
  describe Quadrilateral do
 	let(:quad) {Quadrilateral.new(1,2,3,4)}
 
 	describe "#initialize" do
 		it "should raise an ArgumentError error if no parameters passed" do
-			expect { Quadrilateral.new(4,4,3) }.to raise_erro(ArgumentError)
+			expect { Quadrilateral.new(4,4,3) }.to raise_error(ArgumentError)
 		end
 	end
 
@@ -15,7 +66,6 @@ requier 'spec_helper.rb'
 		perimetr = quad.perimetr
 		expect(perimetr).to eq perim_chkd
 	   end
-
 	end
 	
 	describe "#area" do 
@@ -54,7 +104,7 @@ requier 'spec_helper.rb'
 	end
  end
 
-  describe Rhombus do
+ describe Rhombus do
 	let(:paral) {Rhombus.new(2,90)}
 
 	describe "#initialize" do
@@ -81,6 +131,59 @@ requier 'spec_helper.rb'
 	end
  end
 
+ describe Trapezoid do
+	let(:trap) {Trapezoid.new(2,3,4,5,6)}
+
+	describe "#initialize" do
+		it "should raise an ArgumentError error if no parameters passed" do
+			expect { Trapezoid.new(3) }.to raise_erro(ArgumentError)
+		end
+	end
+
+	describe "#perimetr" do
+	   it "should count perimetr of trapezoid" do
+		perim_chkd = 14
+		perimetr = trap.perimetr
+		expect(perimetr).to eq perim_chkd
+	   end
+
+	end
+	
+	describe "#area" do 
+	   it "should count area of trapezoid" do
+	      area_chkd = 4 
+	      area = trap.area
+	      expect(area).should eq area_chkd
+	   end
+	end
+ end
+
+describe Rectangle do
+	let(:rec) {Rectangle.new(2,3)}
+
+	describe "#initialize" do
+		it "should raise an ArgumentError error if no parameters passed" do
+			expect { Rectangle.new(3) }.to raise_erro(ArgumentError)
+		end
+	end
+
+	describe "#perimetr" do
+	   it "should count perimetr of rectangle" do
+		perim_chkd = 10
+		perimetr = rec.perimetr
+		expect(perimetr).to eq perim_chkd
+	   end
+
+	end
+	
+	describe "#area" do 
+	   it "should count area of rectangle" do
+	      area_chkd = 6 
+	      area = rec.area
+	      expect(area).should eq area_chkd
+	   end
+	end
+ end
 
 
 
